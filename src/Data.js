929922d15957de301f53b753ac761a3040418f3b
HabibRazek <habibrazeg32@gmail.com>
2022-08-18 16:08:23 +0100

    CheckPoint Router

---
+++ src/Data.js
@@ -0,0 +1,38 @@
+export const movieData = [
+    {
+        id: Math.random(),
+        name: "Prey",
+        universe: "Dan Trachtenberg",
+        rating: 2,
+        pictureURL: "https://lumiere-a.akamaihd.net/v1/images/p_prey_246_caa2c8c5.jpeg",
+        description: "Set in the Comanche Nation 300 years ago, is the story of a young woman, Naru, a fierce and highly skilled warrior. She has been raised in the shadow of some of the most legendary hunters who roam the Great Plains, so when danger threatens her camp, she sets out to protect her people.",
+        Trailer: "https://www.youtube.com/embed/wZ7LytagKlc"
+    },
+    {
+        id: Math.random(),
+        name: "Free Guy",
+        universe: "Shawn Levy",
+        rating: 2,
+        pictureURL: "https://lumiere-a.akamaihd.net/v1/images/p_20cs_freeguy_homeent_21930_49e74453.jpeg",
+        description: "In , a bank teller who discovers he is actually a background player in an open-world video game, decides to become the hero of his own story... one he rewrites himself. Now in a world where there are no limits, he is determined to be the guy who saves his world his way... before it is too late.",
+        Trailer: "https://www.youtube.com/embed/X2m-08cOAbc"
+    },
+    {
+        id: Math.random(),
+        name: "Breakthrough",
+        universe: "Roxann Dawson",
+        rating: 5,
+        pictureURL: "https://lumiere-a.akamaihd.net/v1/images/breakthrough_feature-poster_584x800_c23c19b1.jpeg",
+        description: "The film tells the story of a St. Louis teenager who slipped through an icy lake in January 2015 and was underwater for 15 minutes before resuscitative efforts were started. Although being rescued, he is in a coma, and his family must rely on their faith to get through the ordeal. Breakthrough premiered in St.",
+        Trailer: "https://www.youtube.com/embed/52bORzIODec"
+    },
+    {
+        id: Math.random(),
+        name: "Fantastic Four",
+        universe: "Miles Teller",
+        rating: 4,
+        pictureURL: "https://lumiere-a.akamaihd.net/v1/images/image_6ce16244.jpeg",
+        description: "The story features Reed Richards, Ben Grimm, Susan Storm, Johnny Storm and Victor Von Doom getting hit by a space storm after boarding a space station. As a result of the radiation from the storm, they gain new abilities and powers that they have deal with in their own ways.",
+        Trailer: "https://www.youtube.com/embed/_rRoD28-WgU"
+    }
+];
